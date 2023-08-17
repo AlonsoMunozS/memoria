1d3832eaf2df32f8fff9423a4924774953cf9218
Alonso Muñoz Sepulveda <amunoz@simbiotica.ai>
2023-08-17 17:25:12 -0400

    implement application and domain layer to create tender

---
+++ backend/context/tenders/tender/domain/tender.ts
@@ -0,0 +1,56 @@
+export class Tender {
+	readonly id: number
+	readonly name: String
+	readonly safi: String
+	readonly province: string
+	readonly commune: string
+	readonly location?: Array<number>
+	readonly createdAt: number
+	readonly createdBy: number
+	readonly currentStage: String
+	readonly mercadoPublicoId: string
+	readonly category?: string
+	readonly companies?: Array<String>
+
+	constructor({
+        id,
+        name,
+        safi,
+        province,
+        commune,
+        location,
+        createdAt,
+        createdBy,
+        currentStage,
+        mercadoPublicoId,
+        category,
+        companies
+	}: {
+         id: number,
+         name: String,
+         safi: String,
+         province: string,
+         commune: string,
+         location?: Array<number>,
+         createdAt: number,
+         createdBy: number,
+         currentStage: String,
+         mercadoPublicoId: string,
+         category?: string,
+         companies?: Array<String>
+	}) {
+        this.id = id
+        this.name = name
+        this.safi = safi
+        this.province = province
+        this.commune = commune
+        this.location = location
+        this.createdAt = createdAt
+        this.createdBy = createdBy
+        this.currentStage = currentStage
+        this.mercadoPublicoId = mercadoPublicoId
+        this.category = category
+        this.companies = companies 
+	}
+
+}
