78f119c32a03521d10f752598ea302c279c2697e
Alonso Muñoz Sepulveda <amunoz@simbiotica.ai>
2023-08-17 19:08:34 -0400

    implement infrastructure layer

--- backend/context/tenders/tender/application/create/tenderCreator.ts
+++ backend/context/tenders/tender/application/create/tenderCreator.ts
@@ -3,12 +3,12 @@ import { TenderRepository } from "../../domain/tenderRepository";
 import { CreateTenderRequest } from "./createTenderRequest";
 
 
-export class UserCreator {
+export class TenderCreator {
   constructor(
     private readonly tenderRepository: TenderRepository,
   ) {}
 
-  async createUser(request: CreateTenderRequest ): Promise<void> {
+  async createTender(request: CreateTenderRequest ): Promise<void> {
     const tender = new Tender(request)
     await this.tenderRepository.create(tender);
   }
