6ef43cb41e3d5c76ff1fdc91324f16f4ba8f1244
Lesly Fuentes <leslyjfuentes@sansano.usm.cl>
2023-08-18 17:23:34 -0400

    Implement Primereact library for styles

---
+++ frontend/app/pages/companies/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+//import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+//import "primereact/resources/primereact.min.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <Button label="Click Me" />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/app/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'primereact/button';
+//import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+//import "primereact/resources/primereact.min.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>AFUERA</h1>
+      <Button label="Click Me" />
+    </div>
+  );
+};
+
+export default HomePage;
