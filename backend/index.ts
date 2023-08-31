1c80ddfc1a0635935dc57cdd1699f423caf92948
Lesly Fuentes <leslyjfuentes@sansano.usm.cl>
2023-08-31 01:02:00 -0400

    Fix: Express Permission error

--- backend/index.ts
+++ backend/index.ts
@@ -7,6 +7,14 @@ import { tenderRouter } from "./context/tenders/tender/infrastructure/rest-api/t
 function boostrap() {
   const app = express();
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
   app.use(bodyParser.json());
   app.use("/tenders", tenderRouter);
 
