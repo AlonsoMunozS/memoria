5ba60fe6a9565949806bf79808404efc42acf685
Alonso Muñoz Sepulveda <amunoz@simbiotica.ai>
2023-09-21 20:10:56 -0300

    fix mongodb uri

--- backend/context/tenders/tender/infrastructure/tender-repository/mongo-tender-repository.ts
+++ backend/context/tenders/tender/infrastructure/tender-repository/mongo-tender-repository.ts
@@ -2,7 +2,7 @@ import { Tender } from "../../domain/tender";
 import { TenderRepository } from "../../domain/tenderRepository";
 import { MongoClient, ServerApiVersion } from "mongodb"
 
-const uri = "mongodb+srv://Alonso:1234Alonso@pruebapmm.q41p8o6.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb://Alonso:1234Alonso@ac-ouxjhz4-shard-00-00.q41p8o6.mongodb.net:27017,ac-ouxjhz4-shard-00-01.q41p8o6.mongodb.net:27017,ac-ouxjhz4-shard-00-02.q41p8o6.mongodb.net:27017/?replicaSet=atlas-8gtwdq-shard-0&authSource=admin&tls=true";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,12 +16,12 @@ const collectionName = "Tenders";
 
 export class MongoTenderRepository implements TenderRepository {
 
-  async create(tender : Tender): Promise<void> {
+  async create(tender: Tender): Promise<void> {
     try {
       await client.connect();
       const collection = database.collection(collectionName);
       await collection.insertOne(tender);
-    }finally{
+    } finally {
       await client.close();
     }
   }
@@ -32,34 +32,35 @@ export class MongoTenderRepository implements TenderRepository {
       const tendersCursor = collection.find({});
       const tendersArray = await tendersCursor.toArray();
       const mappedTenders: Tender[] = tendersArray.map((tenderDoc: any) => {
-        return new Tender({   
-          id:tenderDoc.id,
-          name:tenderDoc.name,
-          safi:tenderDoc.safi,
-          province:tenderDoc.province,
-          commune:tenderDoc.commune,
-          address:tenderDoc.address,
-          location:tenderDoc.location,
-          createdAt:tenderDoc.createdAt,
-          createdBy:tenderDoc.createdBy,
-          currentStage:tenderDoc.currentStage,
-          mercadoPublicoId:tenderDoc.mercadoPublicoId,
-          category:tenderDoc.category,
-          companies:tenderDoc.companies});
+        return new Tender({
+          id: tenderDoc.id,
+          name: tenderDoc.name,
+          safi: tenderDoc.safi,
+          province: tenderDoc.province,
+          commune: tenderDoc.commune,
+          address: tenderDoc.address,
+          location: tenderDoc.location,
+          createdAt: tenderDoc.createdAt,
+          createdBy: tenderDoc.createdBy,
+          currentStage: tenderDoc.currentStage,
+          mercadoPublicoId: tenderDoc.mercadoPublicoId,
+          category: tenderDoc.category,
+          companies: tenderDoc.companies
+        });
       });
       return mappedTenders;
-    }finally{
+    } finally {
       await client.close();
     }
   }
 
-  async findById(tenderId : number): Promise<Tender> {
+  async findById(tenderId: number): Promise<Tender> {
     try {
       await client.connect();
       const collection = database.collection(collectionName);
       // const document = await collection.findOne({ _id: objectId });
       const tenderFound = await collection.findOne({ id: tenderId });
-      const tender = new Tender({   
+      const tender = new Tender({
         id: tenderFound?.id,
         name: tenderFound?.name,
         safi: tenderFound?.safi,
@@ -72,21 +73,22 @@ export class MongoTenderRepository implements TenderRepository {
         currentStage: tenderFound?.currentStage,
         mercadoPublicoId: tenderFound?.mercadoPublicoId,
         category: tenderFound?.category,
-        companies: tenderFound?.companies});
+        companies: tenderFound?.companies
+      });
       return tender;
-    }finally{
+    } finally {
       await client.close();
     }
   }
-  async remove(tenderId : number): Promise<void> {
+  async remove(tenderId: number): Promise<void> {
     try {
       await client.connect();
       const collection = database.collection(collectionName);
       await collection.deleteOne({ id: tenderId });
-    }finally{
+    } finally {
       await client.close();
     }
   }
-  
-  
+
+
 }
