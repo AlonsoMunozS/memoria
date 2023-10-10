850f75d8523a7f40f67a0c26a6dbb4e576b325b6
Alonso Muñoz Sepulveda <amunoz@simbiotica.ai>
2023-10-09 23:14:41 -0300

    fix user dependencies

--- backend/api-rest/usersDependencies.ts
+++ backend/api-rest/usersDependencies.ts
@@ -1,6 +1,6 @@
 import { UserCreator } from "../context/users/user/application/create/userCreator";
 import { UserFinder } from "../context/users/user/application/findById/userFinder";
-import { UserLoginer } from "../context/users/user/application/singIn/userLoginer";
+import { UserLoginer } from "../context/users/user/application/signIn/userLoginer";
 import { PasswordUpdater } from "../context/users/user/application/updatePassword/passwordUpdater";
 import { FirebaseUserAuth } from "../context/users/user/infrastructure/firebase-user-Auth";
 import { MongoUserRepository } from "../context/users/user/infrastructure/mongo-user-repository";
