5394b0cec13c6664a4f13fb974ea43324fce8055
Lesly Fuentes <lesly.fuentes@sansano.usm.cl>
2023-10-10 15:21:47 -0300

    implement part 1 general & stages info

--- frontend/app/pages/tenders/tender/[id]/index.tsx
+++ frontend/app/pages/tenders/tender/[id]/index.tsx
@@ -2,9 +2,13 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../../components/Layout";
 import HomeBar from "../../../components/HomeBar";
+import GeneralInfo from "./components/GeneralInfo";
+import { Card } from "primereact/card";
+import StagesInfo from "./components/StagesInfo";
 const Tender = () => {
     const [loggedUser, setLoggedUser] = useState(false);
     const router = useRouter();
+    const { id } = router.query;
 
     useEffect(() => {
         if (localStorage.getItem('authToken') == null) {
@@ -21,6 +25,11 @@ const Tender = () => {
                     <Layout>
                         <HomeBar />
                     </Layout>
+                    <Card title={`Licitación N°: ${id}`}>
+                        <GeneralInfo />
+                        <StagesInfo />
+                    </Card>
+
                 </div>
             )}
 
