98d19bf85e1018bee560db015b7589f8832938b7
Lesly Fuentes <lesly.fuentes@sansano.usm.cl>
2023-10-10 01:12:48 -0300

    Verify token in tender

--- frontend/app/pages/tenders/tender/[id]/index.tsx
+++ frontend/app/pages/tenders/tender/[id]/index.tsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Layout from "../../../components/Layout";
+import HomeBar from "../../../components/HomeBar";
 const Tender = () => {
+    const [loggedUser, setLoggedUser] = useState(false);
+    const router = useRouter();
 
+    useEffect(() => {
+        if (localStorage.getItem('authToken') == null) {
+            router.push('/login');
+        }
+        else {
+            setLoggedUser(true);
+        }
+    }, []);
     return (
         <div>
-            Hola
+            {loggedUser && (
+                <div>
+                    <Layout>
+                        <HomeBar />
+                    </Layout>
+                </div>
+            )}
+
         </div>
+
     );
 }
 export default Tender;
