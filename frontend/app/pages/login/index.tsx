b44f331c51892d95950e4584e9925b02f315d9a1
Lesly Fuentes <lesly.fuentes@sansano.usm.cl>
2023-11-09 08:43:37 -0300

    login with exist user

--- frontend/app/pages/login/index.tsx
+++ frontend/app/pages/login/index.tsx
@@ -1,8 +1,16 @@
 import { Card } from "primereact/card";
 import LoginForm from "./components/LoginForm";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 
 export const Login = () => {
+    const router = useRouter();
+    useEffect(() => {
+        if (localStorage.getItem('authToken') !== null) {
+            router.push('/');
+        }
+    }, []);
 
     return (
         <div className="background">
